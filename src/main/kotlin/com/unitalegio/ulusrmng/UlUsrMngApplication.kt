1525ef781c02f7659338fe0e64c15935a1555b6b
Max Pestov <pestov.maxik@gmail.com>
2023-01-26 20:25:09 +0400

    Spring application initialized

---
+++ src/main/kotlin/com/unitalegio/ulusrmng/UlUsrMngApplication.kt
@@ -0,0 +1,11 @@
+package com.unitalegio.ulusrmng
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UlUsrMngApplication
+
+fun main(args: Array<String>) {
+    runApplication<UlUsrMngApplication>(*args)
+}
---
+++ src/test/kotlin/com/unitalegio/ulusrmng/UlUsrMngApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.unitalegio.ulusrmng
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UlUsrMngApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
